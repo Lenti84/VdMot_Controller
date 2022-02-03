507b070b6308643afd5526560eda5361af0a43b7
Gargano <70272783+SurfGargano@users.noreply.github.com>
2022-02-03 14:47:51 +0100

    website improved stm update: hide update table during file upload remove favicon link from esp update

--- software_esp32/WebUpdateEsp/ui/compress.js
+++ software_esp32/WebUpdateEsp/ui/compress.js
@@ -12,7 +12,8 @@ const HTML = `
     <meta charset=utf-8>
     <meta http-equiv=X-UA-Compatible content="IE=edge">
     <meta name=viewport content="width=device-width,initial-scale=1">
-    <link rel=icon href=/favicon.ico> <title>WT32 Update OTA</title>
+    <link rel="icon" href="data:,"> 
+	<title>WT32 Update OTA</title>
  
 </head>
 
