2e73139f28b221c880236cdf8dc0f77cbe202737
Gargano <70272783+SurfGargano@users.noreply.github.com>
2023-03-29 17:44:56 +0200

    favicon added

--- software_esp32/web_pages_source/zz.sh
+++ software_esp32/web_pages_source/zz.sh
@@ -4,6 +4,7 @@ mkdir ../web_pages
 echo "gzipping files..."
 gzip -9 -f -c ./h/index.html > ../web_pages/index.html
 gzip -9 -f -c ./h/stmupdate.html > ../web_pages/stmupdate.html
+gzip -9 -f -c ./h/favicon.ico > ../web_pages/favicon.ico
 //cp ./h/index.html ../web_pages/index.html
 echo "making tfs files..."
 ./mktfs ../web_pages
