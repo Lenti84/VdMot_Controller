e6034749b421fe3374024d56b2c3a5da0b08074f
Gargano <70272783+SurfGargano@users.noreply.github.com>
2023-03-31 13:52:08 +0200

    STM32 : define serial buffer size to 1024. Because stm is 32 bit mcu this should not an issue: buffer size > 256 bytes

--- software_stm32/src/communication.cpp
+++ software_stm32/src/communication.cpp
@@ -28,8 +28,8 @@
   Copyright (C) 2021 Lenti84  https://github.com/Lenti84/VdMot_Controller
 *END************************************************************************/
 
-#define SERIAL_TX_BUFFER_SIZE 256
-#define SERIAL_RX_BUFFER_SIZE 256
+#define SERIAL_TX_BUFFER_SIZE 1024
+#define SERIAL_RX_BUFFER_SIZE 1024
 
 #include <Arduino.h>
 #include "hardware.h"
